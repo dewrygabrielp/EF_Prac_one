55d1cdd6eed5deb91734416eb0c62ffd1cf37032
dewrygabrielp <dewrygabrielp@gmail.com>
2022-09-21 22:47:31 -0400

    First commit initial

---
+++ Infraestructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeEntityCore_One.Models;
+
+namespace PracticeEntityCore_One.Infraestructure.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+            
+        }
+        //migration add-migration "name the migration"
+        //update-database: para efectuar los cambios de vs a la db
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        //Cuando crear migraciones: cuando se crea una nueva clase 
+        // Cuando agregue una nueva propiedad, que equivale a crear un campo nuevo en la bd
+        //Cuando modifique un valor de campo en la clase (modificar campo en la db)
+
+
+
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeEntityCore_One.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
